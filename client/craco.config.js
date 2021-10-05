5dcd0b95dee0b0271a276320dfcaac06b2cd4df4
Quintus <quintusflannagan@gmail.com>
2021-10-05 14:21:00 -0400

    ignore image folder when hot reloading

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,6 +1,11 @@
-// disable HMR
+const path = require('path')
+// ignore image folder when hot reloading app in dev
 module.exports = {
     devServer: {
-        liveReload: false,
-    },
+        watchOptions: {
+            ignored: [
+                path.resolve(__dirname, 'public/images') // image folder path 
+            ]
+        }
+    }
 };
